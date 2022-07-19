1b868805ea12333d3ace1d0f51f3c7493d08a65d
Dnyaneshwar Choure <100285778+dchoure1@users.noreply.github.com>
2022-07-20 00:09:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
+    <div id="main">
+    <h1 data-ns-test="project-name">Resource Website</h1>
+    <p data-ns-test="project-description">I want to make my own resource website where anyone can find great resources</p>
+    </div>
+  );
 }
 
 
